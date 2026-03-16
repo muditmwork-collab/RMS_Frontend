082f2cded2738e1f55241725f7db3ac745b00896
unsparshdev-prog <unsparsh.dev@gmail.com>
2026-03-16 18:01:52 +0530

    Added hr panal and feature - Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { CareerComponent } from './pages/career/career.component';
+import { HrPanelComponent } from './pages/hr-panel/hr-panel.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'career', component: CareerComponent },
+  { path: 'hr-panel', component: HrPanelComponent },
   { path: '**', redirectTo: '' }
 ];
